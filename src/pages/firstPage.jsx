bd641e78c103a6610d697b1d38dc8604d7326d92
seni95 <ghkdtpdus95@naver.com>
2023-02-13 01:17:49 +0900

    skills hover effect done

--- src/pages/firstPage.jsx
+++ src/pages/firstPage.jsx
@@ -51,22 +51,66 @@ export default function FirstPage() {
       
       </li>
     <li>
-    <DescShow title="CSS" desc={<div> 완성된 HTML태그들에 CSS를 덧입혀 원하는 모습대로 웹 페이지를 디자인 할 수 있습니다.</div>}>
-      
+    <DescShow title="JS" desc={<div> JS를 이용하여 완성된 웹 페이지 화면에 동적인 이펙트들을 자유자재로 구현할 수 있습니다.<br></br>
+    또한 FIREBASE를 이용해 데이터베이스를 다루거나 서버와 통신을 할 때 이 언어를 주로 사용합니다.</div>}>
       <DiJavascript className='text-[#f9ca24]'></DiJavascript>
+      </DescShow>
+      </li>
+    <li>
+    <DescShow title="Firebase" desc={<div>  Firebase 프로젝트를 만들고, 웹에 등록하여 각종 사용자 인증과 관련된 기능을 구현하거나
+    실시간 데이터베이스 기능을 이용하여 데이터를 저장하고 기록할 수 있는 기능을 구현할 수 있습니다.</div>}>
+      <SiFirebase className='text-[#f6e58d]'/>
+      </DescShow>      
+      </li>
+    <li>
+    <DescShow title="JS" desc={<div> 리액트를 이용해 웹 UI/UX를 구현 할 수 있습니다. 또한 React-Router나 React-Query와 같은 수많은 리액트 기반의 라이브러리를 이용하여
+사용자와 상호작용 하게 하거나 서버와 통신 하는 등의 고차원적인 기능을 수행 할 수 있습니다.</div>}>
+      <RiReactjsFill className='text-[#34ace0]'></RiReactjsFill>
+      </DescShow>      
+      </li>
+    <li>
+    <DescShow title="JS" desc={<div> 원하는 페이지를 리액트 네이티브로 빌드해 어플리케이션을 만들 수 있습니다.</div>}>
+      <TbBrandReactNative/>
+      </DescShow>
+      </li>
+
+    <li>
+    <DescShow title="Netlify" desc={<div> 완성한 웹 사이트를 빌드하고 실시간 배포 기능을 사용 할 수 있습니다.</div>}>
+      
+      <SiNetlify className='text-[#2bcbba]'></SiNetlify>
       </DescShow>
       
       </li>
-    <li><SiFirebase className='text-[#f6e58d]'/></li>
-    <li><RiReactjsFill className='text-[#34ace0]'></RiReactjsFill></li>
-    <li><TbBrandReactNative/></li>
-    <li><SiNetlify className='text-[#2bcbba]'></SiNetlify></li>
-    <li><SiMaterialui className='text-[#3867d6]'></SiMaterialui></li>
-    <li><SiJquery className='text-[#4b7bec]'></SiJquery></li>
-    <li><FaYarn className='text-[#4b6584]'></FaYarn></li>
-    <li><SiAdobexd className='text-[#c44569]'></SiAdobexd></li>
-    <li><SiTailwindcss className='text-[#778beb]'></SiTailwindcss></li>
-    <li><SiPostcss className='text-[#ff5e57]'></SiPostcss></li>
+    <li>
+    <DescShow title="MUI" desc={<div> 빠르게 원하는 형태의 UI를 구현할 수 있습니다.</div>}>
+      <SiMaterialui className='text-[#3867d6]'></SiMaterialui>
+      </DescShow>
+      </li>
+    <li>
+    <DescShow title="Jquery" desc={<div> JS보다 짧은 코드로 웹 페이지 이펙트를 간단히 구현 할 수 있습니다.</div>}>
+
+      <SiJquery className='text-[#4b7bec]'></SiJquery>
+      </DescShow>
+      
+      </li>
+    <li>
+    <DescShow title="Jquery" desc={<div> 구현 할 웹 페이지를 디자인 하거나 기획 할 때 사용 합니다.</div>}>
+      
+      <SiAdobexd className='text-[#c44569]'></SiAdobexd>
+      </DescShow>
+      </li>
+    <li>
+    <DescShow title="Jquery" desc={<div> 빠르고 간단하게 CSS를 다룰 수 있습니다.</div>}>
+      <SiTailwindcss className='text-[#778beb]'></SiTailwindcss>
+      </DescShow>
+      </li>
+    <li>
+    <DescShow title="Jquery" desc={<div> 만들려는 서비스의 UI가 복잡하거나, UI간의 상호 호환성이 떨어지는 경우에 주로 사용합니다.</div>}>
+
+      <SiPostcss className='text-[#ff5e57]'></SiPostcss>
+      </DescShow>
+
+      </li>
     </ul>
       </div>
     </div>
