05612fc6cf75c6dca4332e00b0d1ed64e38dc2ef
seni95 <ghkdtpdus95@naver.com>
2023-02-20 17:41:11 +0900

    finish project

--- src/Contents/SelfIntroduction.jsx
+++ src/Contents/SelfIntroduction.jsx
@@ -4,7 +4,24 @@ import {FcCloseUpMode, FcBusinesswoman,FcMms} from 'react-icons/fc';
 
 export default function SelfIntroduction() {
   return [
-    {title:"나의 이야기",
+    {title:"자기 소개",
+content:<Typography>
+안녕하세요. 프론트엔드 개발자를 꿈꾸는 황세연입니다.<br></br>
+계명대학교에서 영어영문학과와 컴퓨터 공학을 복수전공하였고, 졸업 후에는 리액트 기반의 프론트엔드 개발을 이어나가고 있습니다. <br></br>
+<br></br>
+
+<div>아름다운 디자인과 더불어 가시성을 중요하게 여깁니다.</div> 프론트엔드 개발자의 주요 역할은 사용자에게 데이터를 보여주는 일입니다. 
+따라서 사용자가 시각적으로 만족감을 느낄 수 있도록 디자인적으로 신경을 씀과 동시에 
+제공받는 데이터가 사용자의 눈에 확실히 들어와야한다고 생각합니다. 따라서 프로젝트를 할 때, 여러 다른 디자인을 참고하며, 동시에 adobe xd를 이용해 틀을 미리 잡아보는 등 여러 노력을 하고 있습니다. <br></br>
+<br></br>
+
+<div>프로그램의 안정성과 성장에 대해 항상 고민합니다.</div>
+백엔드와의 협업이 매끄럽게 진행되도록 하기 위해서 서버와의 통신을 연구하고 방대한 양의 데이터를 다루거나 사용자와의 매끄러운 상호작용을 위해 공부중에 있습니다.
+또한 공부한 내용을 프로젝트에 도입하여 사용자 인증을 통한 로그인을 구현하거나 데이터베이스 연동을 통한 서비스 제공을 구현하였습니다.
+
+</Typography>,
+avatar:<FcMms className='text-3xl'></FcMms>},
+    {title:"성장 과정",
     content:<Typography>
         처음 개발에 흥미를 가지게 된건 24살 한 인터넷 업체에서 아르바이트를 시작하면서부터였습니다. 당시 html을 처음 알게 되고, 그것을 통해 소규모 웹사이트를 개발해보면서 단순히 어려울것이라고만 생각했던 개발이 생각보다 쉽고, 흥미에도 맞다는 생각을 하게 되었습니다. 그래서 복학을 하면서 컴퓨터공학 복수전공을 신청하게 되었습니다. <br></br>
 
@@ -20,16 +37,17 @@ export default function SelfIntroduction() {
 
 “타로카드 리딩” 이라는 업무 경험은 언뜻, 개발과는 상관 없어 보일 수 있습니다. 하지만 저는 이 아르바이트를 하면서 그 누구보다 “사람”을 잘 이해하게 되었고, 의사소통 과정에서 무엇이 중요한지를 배울 수 있었습니다. 그리고 이것이 개발자로 근무하며 협업과 “소통”에 큰 도움을 줄 것이라는 것을 의심하지 않습니다.
 </Typography>,avatar:<FcCloseUpMode className='text-3xl'></FcCloseUpMode>},
-// {title:"직무 경험",
-// content:<Typography>
-// HTML/CSS/JS를 익혔을 때, 소소한 플레시 게임을 만들거나 원하는 느낌의 대략적인 UI를 구현하면서, 사실 "꼭 Vue나 React와 같은 프레임워크를 익힐 필요가 있을까?"
-// </Typography>,
-// avatar:<FcMms className='text-3xl'></FcMms>},
-//     {title:"입사 후 포부",
-//   content:<Typography>
 
-//   </Typography>,
-//   avatar:<FcBusinesswoman className='text-3xl'></FcBusinesswoman>},
+    {title:"앞으로의 계획",
+  content:<Typography>
+    현재 단기적인 계획으로는 Node.js로 백엔드를 더 공부하여 어떻게하면 프론트와 백이 협업을 더 잘 할수있을지 고민해보고 
+    프론트엔드 단에서 서버에 부담을 줄이기 위해 도움을 줄 수 있는 부분에 대해서도 더 공부를 해보고 싶습니다. <br></br><br></br>
+
+    장기적으로는 꾸준한 학습과 연구를 통해 탄탄한 개발 지식을 쌓고 능숙한 개발자가 되는 것입니다. 
+    동시에 협업하는 구성원들과의 소통에도 많은 노력을 기울이면서 갖게된 원활한 의사소통 능력을 기반으로
+    제가 맡게되는 프로젝트나 업무들을 능숙하게 해쳐 나가는 개발자, 누구나 믿고 프로젝트를 맡기고 싶어하는 개발자가 되고 싶습니다.
+  </Typography>,
+  avatar:<FcBusinesswoman className='text-3xl'></FcBusinesswoman>},
 
   ]
 }
